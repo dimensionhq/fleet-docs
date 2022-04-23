9557595239d5112356a1fca46ac3728a9ee2aa06
Varun Potti <potti.varun07@gmail.com>
2022-04-23 09:35:28 +0530

    fix: installation commands

--- src/components/docs/pages/getting-started/index.tsx
+++ src/components/docs/pages/getting-started/index.tsx
@@ -6,7 +6,7 @@ const GettingStarted = () => {
             <h1 class="text-6xl font-extrabold">Get Started</h1>
             <h3 class="text-2xl mt-7">Installing fleet is easy!</h3>
             <div class="rounded-xl bg-brown-200 w-full max-h-fit p-5">
-                {">"} curl https://cdn.dimension.dev/dimension/fleet/bin/installer.sh | sh
+                {">"} curl -L get.fleet.rs | sh
             </div>
             <Link class="mt-9" href="/docs/start/benchmarks">
                 <div class="w-full min-h-fit border p-5 rounded-xl border-red-200 flex flex-col gap-2 hover:border-red-100">
